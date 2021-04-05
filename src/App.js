2a8644dcad7aba654b31d304337abb1daa2fb6e9
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-04-04 20:21:03 -0400

    add react form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React form practice</h1>
+      <form>
+        <label>
+          Name: <input type='text' name='name' value='' />
+        </label>
+        <br />
+        <label>
+          Email: <input type='text' name='email' value='' />
+        </label>
+        <br />
+        <input type='submit' />
+      </form>
+    </>
   );
 }
 
